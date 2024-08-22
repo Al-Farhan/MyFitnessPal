11aa20f42c490c0e3adadc90c13b0453428b867f
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-08-22 15:56:25 +0530

    add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      {/* Food item view */}
+      <View
+        style={{
+          backgroundColor: "gainsboro",
+          padding: 10,
+          borderRadius: 5,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <View style={{ flex: 1, gap: 5 }}>
+          <Text style={{ fontWeight: "bold", fontSize: 16 }}>Pizza Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi. Accusamus iste nesciunt debitis maiores asperiores sapiente, dolorem magni! Temporibus? </Text>
+          <Text style={{ color: "dimgray" }}>350 cal, Dominos</Text>
+        </View>
+        <AntDesign name="pluscircleo" size={24} color="royalblue" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 10,
+  },
+});
---
+++ src/components/FoodLogListItem.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FoodLogListItem = () => {
+  return (
+    <View>
+      <Text>FoodLogListItem</Text>
+    </View>
+  )
+}
+
+export default FoodLogListItem
+
+const styles = StyleSheet.create({})
